3f92ccbd1eb296437e180deb61db431d44d787d0
Macmod <zz.mcmd@gmail.com>
2023-03-14 14:42:26 -0300

    Increasing default timeout.

--- goblob.go
+++ goblob.go
@@ -71,7 +71,7 @@ func main() {
 		"Maximum of container pages to traverse looking for blobs",
 	)
 	timeout := flag.Int(
-		"timeout", 20,
+		"timeout", 60,
 		"Timeout for HTTP requests (seconds)",
 	)
 	max_idle_conns := flag.Int(
